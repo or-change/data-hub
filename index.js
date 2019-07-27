33fb9786c220ac23aff5c5d11abcde9919d8847a
lichaozhy <lichao@or-change.cn>
2019-07-27 15:16:40 +0800

    [ADDED] Blob [ADDED] Date

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const DataHub = require('./src/hub');
+const Type = require('./src/type');
 
+DataHub.Blob = Type.Blob;
 DataHub.create = function createDataHub(options) {
 	return new DataHub(options);
 };
--- src/type/native.js
+++ src/type/native.js
@@ -6,36 +6,61 @@ module.exports = function install(Type) {
 					type: 'date'
 				};
 				
-				return {
-					type: 'date'
-				};
+				return finalOptions;
 			};
 		},
-		Validator() {
-			return function validator(options) {
-
+		Validator(options) {
+			return function validator(data) {
+				if (!(data instanceof Date)) {
+					throw new Error('A date object expected.');
+				}
 			};
 		},
 		Accessor() {
-
+			return function access(dataNode) {
+				return dataNode;
+			};
 		}
 	});
+
+	const Blob = Type.Blob = class DataHubBlob {
+		constructor(type, buffer) {
+			this.mimeType = type;
+			this.buffer = Buffer.from(buffer);
+		}
+
+		get base64() {
+			return this.buffer.toString('base64');
+		}
+	};
+
+	const blobProxyOptions = {
+		set() {
+			throw new Error('illegal access.');
+		}
+	};
 
 	Type.define('blob', {
 		Normalizer() {
-
+			return function normalize(options) {
+				const finalOptions = {
+					type: 'blob'
+				};
+				
+				return finalOptions;
+			};
 		},
 		Validator() {
-
+			return function validator(data) {
+				if (!(data instanceof Blob)) {
+					throw new Error('A blob expected.');
+				}
+			};
 		},
 		Accessor() {
-
+			return function access(dataNode) {
+				return new Proxy(dataNode, blobProxyOptions);
+			};
 		}
 	});
-
-	Type.Blob = class Blob {
-		constructor(buffer, contentType) {
-			
-		}
-	};
 };
